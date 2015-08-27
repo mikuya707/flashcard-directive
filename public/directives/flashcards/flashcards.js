61d9710dbc34ac90572095a55bdb602a1e111c14
= <danrlabrie@gmail.com>
2015-08-27 16:26:53 -0400

    almost done!

--- public/directives/flashcards/flashcards.js
+++ public/directives/flashcards/flashcards.js
@@ -1,4 +1,18 @@
-app.directive('flashCard', function(){
-	restrict: 'E',
-	templateUrl: '/directives/flashcards/flashcards.html'
+app.directive('card', function(ScoreFactory){
+	return {
+  restrict: 'E',
+	templateUrl: '/directives/flashcards/flashcards.html',
+  scope: {
+    card: '='
+  },
+  link: function(scope,elem,attr) {
+      scope.answerQuestion = function (answer) {
+      if (scope.card.answered) return;
+      scope.card.answered = true;
+      scope.card.answeredCorrectly = answer.correct;
+      if (answer.correct) ScoreFactory.correct++;
+      else ScoreFactory.incorrect++;
+    };
+  }
+  }
 })
